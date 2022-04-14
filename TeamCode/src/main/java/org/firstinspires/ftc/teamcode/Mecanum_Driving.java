1236f9f49a2f149cf1224611c0406b7cc7e4166d
programming-programmer <mthich15@gmail.com>
2022-04-14 13:51:33 -0700

    Update Mecanum_Driving for the ~~aesthetic~~

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_Driving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_Driving.java
@@ -7,52 +7,49 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
 @TeleOp(name = "Mecanum Driving ඞ", group = "Linear Opmode")
 
 public class Mecanum_Driving extends LinearOpMode {
 
-    Servo servo;
+    DcMotor leftFront;
+    DcMotor backLeft;
+    DcMotor frontRight;
+    DcMotor backRight;
 
-    @Override
-    public void runOpMode() throws InterruptedException {
-        DcMotor leftFront = hardwareMap.dcMotor.get("Front_Left");
-        DcMotor backLeft = hardwareMap.dcMotor.get("Back_Right");
-        DcMotor frontRight = hardwareMap.dcMotor.get("Front_Right");
-        DcMotor backRight = hardwareMap.dcMotor.get("Back_Left");
+    @Override public void runOpMode() {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = dashboard.getTelemetry();
 
+        leftFront = hardwareMap.dcMotor.get("Front_Left");
+        backLeft = hardwareMap.dcMotor.get("Back_Right");
+        frontRight = hardwareMap.dcMotor.get("Front_Right");
+        backRight = hardwareMap.dcMotor.get("Back_Left");
 
-        // Reverse the right side motors
-        // Reverse left motors if you are using NeveRests
+        // Reverse motors
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
-        if (isStopRequested()) return;
-
         while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
 
-            while (opModeIsActive()) {
-                double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-                double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-                double rx = gamepad1.right_stick_x;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
 
-                double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-                double frontLeftPower = (y + x + rx) / denominator;
-                double backLeftPower = (y - x + rx) / denominator;
-                double frontRightPower = (y - x - rx) / denominator;
-                double backRightPower = (y + x - rx) / denominator;
+            leftFront.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
 
-                leftFront.setPower(frontLeftPower);
-                backLeft.setPower(backLeftPower);
-                frontRight.setPower(frontRightPower);
-                backRight.setPower(backRightPower);
-            }
-        }
-    }
         }
     }
 }
+
